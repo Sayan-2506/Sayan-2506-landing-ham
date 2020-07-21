403424341160586016d98fdfa1d4779b09f6bff0
Sayan-2506 <urustemovsayan25.com>
2020-07-21 11:17:05 +0600

    added finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,38 @@
 $(function(){
 
+    $(".say__inner").slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1300,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+        ]
+    });
+
+
+    $('.services__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.services__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.services__tabs .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
+
+
+        $('.footer-btn-list').on('twoclick', function(){
+          $('.footer-btn-list').removeClass('active');
+        });
+    
 
     var mixer = mixitup('.amazing__inner-box');
     
