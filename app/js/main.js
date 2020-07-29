dfc92890d1efc6fbb6cbbaa3966f39de5e76cc46
Sayan-2506 <urustemovsayan25.com>
2020-07-29 16:28:35 +0600

    the end

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,43 @@
 $(function(){
 
+
+  $('.tiger1').on('click', function(){
+    $('.tiger1').toggleClass('active')
+  });
+  $('.tiger2').on('click', function(){
+    $('.tiger2').toggleClass('active')
+  });
+  $('.tiger3').on('click', function(){
+    $('.tiger3').toggleClass('active')
+  });
+  $('.tiger4').on('click', function(){
+    $('.tiger4').toggleClass('active')
+  });
+  $('.tiger5').on('click', function(){
+    $('.tiger5').toggleClass('active')
+  });
+  $('.tiger6').on('click', function(){
+    $('.tiger6').toggleClass('active')
+  });
+  $('.tiger7').on('click', function(){
+    $('.tiger7').toggleClass('active')
+  });
+  $('.tiger8').on('click', function(){
+    $('.tiger8').toggleClass('active')
+  });
+  $('.tiger9').on('click', function(){
+    $('.tiger9').toggleClass('active')
+  });
+  $('.tiger10').on('click', function(){
+    $('.tiger10').toggleClass('active')
+  });
+  $('.tiger11').on('click', function(){
+    $('.tiger11').toggleClass('active')
+  });
+  $('.tiger12').on('click', function(){
+    $('.tiger12').toggleClass('active')
+  });
+  
     $(".say__inner").slick({
         dots: true,
         arrows: true,
@@ -7,10 +45,10 @@ $(function(){
         slidesToScroll: 1,
         responsive: [
           {
-            breakpoint: 1300,
+            breakpoint: 380,
             settings: {
               slidesToShow: 1,
-              slidesToScroll: 1,
+              slidesToScroll: 2,
               infinite: true,
               dots: true
             }
@@ -31,6 +69,10 @@ $(function(){
 
         $('.footer-btn-list').on('twoclick', function(){
           $('.footer-btn-list').removeClass('active');
+        });
+
+        $('.menu__btn').on('click', function(){
+          $('.menu_list').slideToggle()
         });
     
 
